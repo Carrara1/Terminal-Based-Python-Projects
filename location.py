4d8065438a83ba625b305c96e84beeacd6f8ed71
Joao Carrara <106992893+Carrara1@users.noreply.github.com>
2022-10-04 00:53:24 -0300

    added file locations/location.py, simplified the menu
    moved the file locations to location.py

---
+++ location.py
@@ -0,0 +1,14 @@
+hangman = "Terminal-Based-Python-Projects/files/caesar cipher/encoding.py"
+caesarcipher = "Terminal-Based-Python-Projects/files/hangman/main.py"
+bmicalculator = "Terminal-Based-Python-Projects/files/"
+passwordgenerator = "Terminal-Based-Python-Projects/files/"
+tipcalculator = "Terminal-Based-Python-Projects/files/tipcalculator.py"
+coffeemachine = "Terminal-Based-Python-Projects/files/coffee machine/main.py"
+rockpaperscissors = "Terminal-Based-Python-Projects/files/"
+pizzadelivery = "Terminal-Based-Python-Projects/files/"
+bandnamegenerator = "Terminal-Based-Python-Projects/files/"
+leapyearchecker = "Terminal-Based-Python-Projects/files/"
+treasurerpg = "Terminal-Based-Python-Projects/files/"
+treasuremap = "Terminal-Based-Python-Projects/files/"
+
+location = [hangman, caesarcipher, bmicalculator, passwordgenerator, tipcalculator, coffeemachine, rockpaperscissors,]
--- main.py
+++ main.py
@@ -3,19 +3,8 @@ def main():
     print("Main Menu")
 if __name__ == '__main__':
     main()
-#---File Locations
-tipcalculator = "Terminal-Based-Python-Projects/files/tipcalculator.py"
-passwordgenerator = ""
-bandnamegenerator = ""
-rockpaperscissors = ""
-treasurerpg = ""
-treasuremap = ""
-pizzadelivery = ""
-bmicalculator = ""
-hangman = ""
-coffeemachine = ""
-caesarcipher = ""
-leapyearchecker = ""
+#---File Locations  ##TODO make a system to go back to main menu after ending each file.
+from location import location
 #---This function will access the files.
 def run_file(path):
     return exec(open(path).read());
@@ -24,25 +13,28 @@ print("Welcome to the Main Menu.\n" "\nList of programs:\n" "[1] To run password
 selection = input("Select which program you want to run:\n")
 #---Extremely simple else/if statement to run the files. 
 if selection == "1":
-    run_file(passwordgenerator)
+    run_file(location[1])
 elif selection == "2":
-    run_file(bmicalculator)
+    run_file(location[2])
 elif selection == "3":
-    run_file(rockpaperscissors)
+    run_file(location[3])
 elif selection == "4":
-    run_file(pizzadelivery)
+    run_file(location[4])
 elif selection == "5":
-    run_file(tipcalculator)
+    run_file(location[5])
 elif selection == "6":
-    run_file(bandnamegenerator)
+    run_file(location[6])
 elif selection == "7":
-    run_file(leapyearchecker)
+    run_file(location[7])
 elif selection == "8":
-    run_file(treasurerpg)
+    run_file(location[8])
 elif selection == "9":
-        run_file(treasuremap)
+    run_file(location[9])
+elif selection == "10":
+    run_file(location[10])
+elif selection == "11":
+    run_file(location[11])
 else:
     print("Wrong option. Try again. \n")
 #------------------------------------------------------------------------------
 
-##TODO add the files, make a system to go back to main menu after ending each file.
